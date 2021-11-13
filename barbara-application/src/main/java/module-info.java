92faabf728ea387684bc2e02e844d86c943256e9
carddamom <carddamom@tutanota.com>
2021-11-13 14:17:41 +0000

    feat: Added the initial maven structure

---
+++ barbara-application/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module barbara.application {
+  requires javafx.graphics;
+  requires org.apache.commons.lang3;
+  requires jakarta.validation;
+  requires org.slf4j;
+  requires noexception; // Automatic module name...
+  requires org.spongepowered.configurate;
+  requires geantyref; // Automatic module name...
+  requires com.dlsc.preferencesfx;
+  requires javax.inject;
+  requires com.google.gson;
+  requires dagger; // Automatic module name...
+}
